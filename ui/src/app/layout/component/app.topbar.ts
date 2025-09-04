290bfbba46aa13e03c17beed7bcdf69d8f629844
Vladimir Novoseltsev <vn@delai-delo.by>
2025-09-04 23:08:53 +0300

    Change title

--- ui/src/app/layout/component/app.topbar.ts
+++ ui/src/app/layout/component/app.topbar.ts
@@ -33,7 +33,7 @@ import { LayoutService } from '../service/layout.service';
                         />
                     </g>
                 </svg>
-                <span>SAKAI</span>
+                <span>WORKBENCH</span>
             </a>
         </div>
 
