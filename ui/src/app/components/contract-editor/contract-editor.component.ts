812b0f005ecff9923f432b7f149811859327af60
Vladimir Novoseltsev <vn@delai-delo.by>
2025-09-05 15:22:44 +0300

    Add stubs for contract and contractor UI

---
+++ ui/src/app/components/contract-editor/contract-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-editor',
+  templateUrl: './contract-editor.component.html',
+  styleUrls: ['./contract-editor.component.css']
+})
+export class ContractEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ui/src/app/components/contractor-editor/contractor-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contractor-editor',
+  templateUrl: './contractor-editor.component.html',
+  styleUrls: ['./contractor-editor.component.css']
+})
+export class ContractorEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
